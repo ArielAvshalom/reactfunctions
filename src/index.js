f1cb94cf9803849deaa6ff95a8317d6b754cd880
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-01-11 09:51:15 -0500

    Added js index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from "./people";
+import FireAlert from "./alertevent";
+import WonChess from "./conditionals";
+import Garage from "./lists"
+import NameForm from "./forms";
+import FormWithHook from "./useStateHook";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+    <Home/>,
+    document.getElementById('root2')
 );
+
+ReactDOM.render(
+    <FireAlert />,
+    document.getElementById('fire')
+)
+
+ReactDOM.render(
+    <WonChess wonGame={-Infinity} />,
+    document.getElementById('chess')
+)
+
+ReactDOM.render(
+    <Garage />, document.getElementById('garage')
+)
+
+ReactDOM.render(
+    <NameForm />,
+    document.getElementById('nameForm')
+)
+
+ReactDOM.render(
+    <FormWithHook />, document.getElementById('hookedForm')
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
